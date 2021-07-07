99be2cda5cf1b2ca3a4256e59d5b36c6e38156ca
Admin <Admin@DESKTOP-KFVUNS7>
2021-07-07 10:23:14 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()//executing results
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
+            var response = CallUrl(url).Result;
+            var linkList = ParseHtml(response);
+            WriteToTxt(linkList);
+            return View();
+        }
+        private static async Task<string> CallUrl(string fullUrl)//making the request
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private List<string> ParseHtml(string html)// restricting the results
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                    .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection")).ToList();
+
+            List<string> wikiLink = new List<string>();
+
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0)
+                    wikiLink.Add("https://en.wikipedia.org/" + link.FirstChild.Attributes[0].Value);
+            }
+
+            return wikiLink;
+
+        }
+
+        private void WriteToTxt(List<string> links)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var link in links)
+            {
+                sb.AppendLine(link);
+            }
+
+            System.IO.File.WriteAllText("links.txt", sb.ToString()); //chaange file extension to csv if needeed
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
